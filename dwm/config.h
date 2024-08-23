6cb7a281e7bd3fc7454977c36a257c4b211820c7
yusufkhalifadev <yusufkhalifadev@gmail.com>
2024-08-23 21:12:20 +0300

    cleanup: added .zsh

--- dwm/config.h
+++ dwm/config.h
@@ -11,13 +11,10 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMonoNerdFont:size=10" };
-//static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-//static const char col_gray4[]       = "#eeeeee";
 static const char col_gray4[]       = "#bcbcbc";
-//static const char col_cyan[]        = "#005577";
 static const char col_cyan[]        = "#4c566a";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -34,7 +31,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class         instance      title      tags mask   iscentered   isfloating   monitor */
-	{ "st",          "st",         "st",      0,          1,           0,           -1 },
+	{ "alacritty",   "alacritty",  "alacritty",      0,          1,           0,           -1 },
 	{ NULL,          NULL,         NULL,      0,          0,           0,           -1 },
 };
 
@@ -66,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "rofi", "-show", "run", NULL };
+static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
